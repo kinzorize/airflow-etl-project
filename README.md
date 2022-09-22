7054e8cda04bbb333ff4019805de828289f78182
kenzo2017 <mustardseed19@gmail.com>
2022-09-23 01:00:10 +0300

    work in progress

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ In the project, i created an end-to-end data engineering project using airflow, 
 - Pandas
 - tweepy : I used this package to access the twitter data
 - s3fs : used to read, store and write data on s3 bucket.
+
+# Note
+
+- for you to access the "Twitter API v2" you need to upgrade from Essential to Elevated for free.
--- twitter_etl.py
+++ twitter_etl.py
@@ -4,10 +4,10 @@ import json
 from datetime import datetime
 import s3fs
 
-access_key = ""
-access_secret = ""
-consumer_key = ""
-consumer_secret = ""
+access_key = "u88LUzIHUXUrMV1yGTDKLs1oe"
+access_secret = "8VSf84NVbtj7L1UQ399Faal3vveq7tKwbgusHeRSGAZ8UDpsP2"
+consumer_key = "1070169780-bzC64vQOP6LKFGXECbTNZB8vNfZ4O3XcN3nCCeC"
+consumer_secret = "7TStZjgthxGDviEho6UbidXit4DHIuq5xu2n47NTjI8z0"
 
 # Twitter authentication
 auth = tweepy.OAuthHandler(access_key, access_secret)
@@ -21,4 +21,16 @@ tweets = api.user_timeline(screen_name='@elonmusk',
                            include_rts=False,
                            tweet_mode='extended'  # Necessary to keep full text
                            )
-print(tweets)
+tweet_list = []
+for tweet in tweets:
+    text = tweet._json["full_text"]
+
+    refined_tweet = {"user": tweet.user.screen_name,
+                     'text': text,
+                     'favorite_count': tweet.favorite_count,
+                     'retweet_count': tweet.retweet_count,
+                     'created_at': tweet.created_at}
+
+    tweet_list.append(refined_tweet)
+df = pd.DataFrame(tweet_list)
+df.to_csv("elonmusk_twitter_data.csv")
