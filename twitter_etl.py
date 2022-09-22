3b98d1d6bc90dbdc89643413fd073fe89da926d3
kenzo2017 <mustardseed19@gmail.com>
2022-09-22 03:27:11 +0300

    work-in-progress

--- twitter_etl.py
+++ twitter_etl.py
@@ -4,10 +4,10 @@ import json
 from datetime import datetime
 import s3fs
 
-access_key = "u88LUzIHUXUrMV1yGTDKLs1oe"
-access_secret = "8VSf84NVbtj7L1UQ399Faal3vveq7tKwbgusHeRSGAZ8UDpsP2"
-consumer_key = "1070169780-bzC64vQOP6LKFGXECbTNZB8vNfZ4O3XcN3nCCeC"
-consumer_secret = "7TStZjgthxGDviEho6UbidXit4DHIuq5xu2n47NTjI8z0"
+access_key = ""
+access_secret = ""
+consumer_key = ""
+consumer_secret = ""
 
 # Twitter authentication
 auth = tweepy.OAuthHandler(access_key, access_secret)
