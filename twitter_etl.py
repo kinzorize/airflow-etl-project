aaae0e58e21c8b17a44c554ae8b921b25f2ead48
kenzo2017 <mustardseed19@gmail.com>
2022-09-24 02:34:45 +0300

    Finished Task

--- twitter_etl.py
+++ twitter_etl.py
@@ -4,33 +4,38 @@ import json
 from datetime import datetime
 import s3fs
 
-access_key = "u88LUzIHUXUrMV1yGTDKLs1oe"
-access_secret = "8VSf84NVbtj7L1UQ399Faal3vveq7tKwbgusHeRSGAZ8UDpsP2"
-consumer_key = "1070169780-bzC64vQOP6LKFGXECbTNZB8vNfZ4O3XcN3nCCeC"
-consumer_secret = "7TStZjgthxGDviEho6UbidXit4DHIuq5xu2n47NTjI8z0"
 
-# Twitter authentication
-auth = tweepy.OAuthHandler(access_key, access_secret)
-auth.set_access_token(consumer_key, consumer_secret)
+def run_twitter_etl():
 
-# Create an API object
-api = tweepy.API(auth)
+    access_key = "u88LUzIHUXUrMV1yGTDKLs1oe"
+    access_secret = "8VSf84NVbtj7L1UQ399Faal3vveq7tKwbgusHeRSGAZ8UDpsP2"
+    consumer_key = "1070169780-bzC64vQOP6LKFGXECbTNZB8vNfZ4O3XcN3nCCeC"
+    consumer_secret = "7TStZjgthxGDviEho6UbidXit4DHIuq5xu2n47NTjI8z0"
 
-tweets = api.user_timeline(screen_name='@elonmusk',
-                           count=200,  # maximum count of 200
-                           include_rts=False,
-                           tweet_mode='extended'  # Necessary to keep full text
-                           )
-tweet_list = []
-for tweet in tweets:
-    text = tweet._json["full_text"]
+    # Twitter authentication
+    auth = tweepy.OAuthHandler(access_key, access_secret)
+    auth.set_access_token(consumer_key, consumer_secret)
 
-    refined_tweet = {"user": tweet.user.screen_name,
-                     'text': text,
-                     'favorite_count': tweet.favorite_count,
-                     'retweet_count': tweet.retweet_count,
-                     'created_at': tweet.created_at}
+    # Create an API object
+    api = tweepy.API(auth)
+
+    tweets = api.user_timeline(screen_name='@elonmusk',
+                               count=200,  # maximum count of 200
+                               include_rts=False,
+                               tweet_mode='extended'  # Necessary to keep full text
+                               )
+    tweet_list = []
+    for tweet in tweets:
+        text = tweet._json["full_text"]
+
+        refined_tweet = {"user": tweet.user.screen_name,
+                         'text': text,
+                         'favorite_count': tweet.favorite_count,
+                         'retweet_count': tweet.retweet_count,
+                         'created_at': tweet.created_at}
 
     tweet_list.append(refined_tweet)
-df = pd.DataFrame(tweet_list)
-df.to_csv("elonmusk_twitter_data.csv")
+    df = pd.DataFrame(tweet_list)
+    df.to_csv("elonmusk_twitter_data.csv")
+
+    Login with username: admin  password: 6NsyMDBgEwM44W8w
